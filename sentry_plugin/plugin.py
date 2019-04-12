75b5f79822b133905c385eda65ec324611c2ccf8
Gabriel Smadi <gabriel@smadi.ci>
2019-04-11 21:27:15 -0400

    Working plugin exception catching

--- sentry_plugin/plugin.py
+++ sentry_plugin/plugin.py
@@ -1,44 +1,66 @@
 from argparse import ArgumentParser, _SubParsersAction
 
 import sentry_sdk
+from sentry_sdk import capture_exception
 
+
+from trinity import __version__
 from trinity.extensibility import BaseIsolatedPlugin
 
 class SentryPlugin(BaseIsolatedPlugin):
+    sentry_host: str
+    project_id: str
+    public_key: str
+    protocol: str
 
     @property
     def name(self) -> str:
-        return "Sentry Error Tracking Plugin"
+        return "Sentry Error Reporting Plugin"
 
     def configure_parser(self,
                          arg_parser: ArgumentParser,
                          subparser: _SubParsersAction) -> None:
-        arg_parser.add_argument(
+        sentry_parser = arg_parser.add_argument_group('sentry')
+
+        sentry_parser.add_argument(
             "--sentry-host",
-            action="store_true",
-            help="Snetry host to report errors",
+            help="Sentry host to report errors",
         )
 
-        arg_parser.add_argument(
+        sentry_parser.add_argument(
             "--sentry-project-id",
-            action="store_true",
-            help="Sentry Project ID",
+            help="Sentry project ID",
         )
 
-        arg_parser.add_argument(
+        sentry_parser.add_argument(
             "--sentry-pub-key",
-            action="store_true",
             help="Sentry public key",
         )
 
-        arg_parser.add_argument(
+        sentry_parser.add_argument(
             "--sentry-insecure",
             action="store_true",
             help="Enable insecure error reporting over HTTP",
         )
 
-    def on_ready(self, manager_eventbus: TrinityEventBusEndpoint) -> None:
-        sentry_sdk.init("https://4e71390f88824d0081226a6a83194ed7@sentry.io/1417193")
+    def on_ready(self, manager_eventbus) -> None:
+        args = self.context.args
+
+        self.sentry_host = args.sentry_host or 'sentry.io'
+        self.project_id = args.sentry_project_id
+        self.public_key = args.sentry_pub_key
+        self.protocol = 'https'
+
+        if args.sentry_insecure:
+            self.protocol = 'http'
+
+        init_url = f'{self.protocol}://{self.public_key}@{self.sentry_host}/{self.project_id}'
+        sentry_sdk.init(init_url, release=f'trinity@{__version__}')
+
+        try:
+            self.start()
+        except:
+            capture_exception()
 
     def do_start(self) -> None:
         pass
